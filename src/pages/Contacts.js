9f2ff8544df5207684714353335cbfb26e2565fb
FerayGuluzada <fguluzada11276@ada.edu.az>
2023-12-30 01:07:53 +0400

    Contacts.js

---
+++ src/pages/Contacts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Information</h2>
+      <p>Name: Feray Gulu-zada</p>
+      <p>Email: feray.guluzade@gmail.com</p>
+      <p>Phone: +1234567890</p>
+      <p>Address: 123 Street, City, Country</p>
+    </div>
+  );
+};
+
+export default Contact;
