89227c1f45f72336a0437d549a78ddb350de47fa
FerayGuluzada <fguluzada11276@ada.edu.az>
2024-01-01 17:11:31 +0400

    Flashcards Update

--- src/pages/Flashcards.js
+++ src/pages/Flashcards.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import './Flashcards.css'; 
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Flashcards.css';
 const FlashcardsPage = () => {
+  const [flashcards, setFlashcards] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/cards')
+      .then((res) => {
+        const transformedData = res.data.map((questionItem, index) => ({
+          id: `${index}-${Date.now()}`,
+          question: questionItem.front,
+          answer: questionItem.back,
+        }));
+        setFlashcards(transformedData); 
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
-    <div className="flashcard">
-      <div className="front">
-        <h3>Front of Flashcard</h3>
-      </div>
-      <div className="back">
-        <p>Back of Flashcard</p>
-      </div>
+    <div className="flashcard-container"> {}
+      {flashcards.map((flashcard) => (
+        <div key={flashcard.id} className="flashcard">
+          <div className="front">
+            <h3>{flashcard.question}</h3>
+          </div>
+          <div className="back">
+            <p>Answer: {flashcard.answer}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
-//database to be imported next, have some ideas after research
 export default FlashcardsPage;
