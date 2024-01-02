3fa8556b5ad87a898ed93872e5fa553d34bc9560
FerayGuluzada <fguluzada11276@ada.edu.az>
2024-01-02 22:16:35 +0400

    Update

--- src/pages/Flashcards.js
+++ src/pages/Flashcards.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Flashcards.css';
+
 const FlashcardsPage = () => {
   const [flashcards, setFlashcards] = useState([]);
+  const [newQuestion, setNewQuestion] = useState('');
+  const [newAnswer, setNewAnswer] = useState('');
 
-  useEffect(() => {
+  useEffect(() => 
+  {
+    fetchData();
+  }, []);
+
+  const fetchData = () => 
+  {
     axios
       .get('http://localhost:3001/cards')
       .then((res) => {
@@ -13,15 +22,55 @@ const FlashcardsPage = () => {
           question: questionItem.front,
           answer: questionItem.back,
         }));
-        setFlashcards(transformedData); 
+        setFlashcards(transformedData);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error('Error:', error);
       });
-  }, []);
+  };
+
+  const addCard = () => 
+  {
+    if (newQuestion && newAnswer) 
+    {
+      const newCard = 
+      {
+        front: newQuestion,
+        back: newAnswer,
+      };
+
+      axios
+        .post('http://localhost:3001/cards', newCard)
+        .then((res) => {
+          fetchData();
+          setNewQuestion('');
+          setNewAnswer('');
+        })
+        .catch((error) => {
+          console.error('Error adding card:', error);
+        });
+    } else {
+      alert('Fill in both question and answer fields.');
+    }
+  };
 
   return (
-    <div className="flashcard-container"> {}
+    <div className="flashcard-container">
+      <div className="flashcard-form">
+        <input
+          type="text"
+          placeholder="Question"
+          value={newQuestion}
+          onChange={(e) => setNewQuestion(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Answer"
+          value={newAnswer}
+          onChange={(e) => setNewAnswer(e.target.value)}
+        />
+        <button onClick={addCard}>Add Card</button>
+      </div>
       {flashcards.map((flashcard) => (
         <div key={flashcard.id} className="flashcard">
           <div className="front">
